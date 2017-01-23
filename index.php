432ac18444a1e2d1846534a1d2bad909134fce99
st4 <st4@N159.PromArm.ru>
2017-01-23 10:24:28 +0200

    new commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GIT TEST</title>
+    </head>
+    <body>
+        тут был гит
+        <?php
+        echo "TEST_GIT";
+        // put your code here
+        ?>
+    </body>
+</html>
