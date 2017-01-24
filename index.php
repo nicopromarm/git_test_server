ef6afacae129259616d5dfa3e7a89b6045b6a9e8
st4 <st4@N159.PromArm.ru>
2017-01-24 14:26:10 +0200

    git_test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>GIT TEST_2</title>
     </head>
     <body>
         тут типо новые файлы и всё такое
